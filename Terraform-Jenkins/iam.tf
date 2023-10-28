13989b8181bb66e3ae9d89b43b67cd9d751c5f56
sandeepnaidu <nainala_sandeep@yahoo.com>
2023-10-28 12:24:57 +0530

    Jenkins instance terraform

--- Terraform-Jenkins/iam.tf
+++ Terraform-Jenkins/iam.tf
@@ -2,27 +2,27 @@ resource "aws_iam_role" "test_role" {
   name = "Jenkins-terraform"
 
   assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
     {
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
       },
-    ]
-  }
-  EOF
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
 }
 
-resource "aws_iam_group_policy_attachment" "test_attachment" {
+resource "aws_iam_role_policy_attachment" "example_attachment" {
   role       = aws_iam_role.test_role.name
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
 
-resource "aws_iam_instance_profile" "test_profile" {
-  name  = "Jenkins-terraform"
-  role  = aws_iam_role.test_role.name
+resource "aws_iam_instance_profile" "example_profile" {
+  name = "Jenkins-terraform"
+  role = aws_iam_role.test_role.name
 }
