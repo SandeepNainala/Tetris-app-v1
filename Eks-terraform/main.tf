1f57f30e0871c245bd223045aea14a69abe68473
sandeepnaidu <nainala_sandeep@yahoo.com>
2023-10-29 13:02:09 +0530

    Jenkins terraform

--- Eks-terraform/main.tf
+++ Eks-terraform/main.tf
@@ -61,7 +61,7 @@ resource "aws_eks_cluster" "example" {
   name     = "EKS_cloud"
   role_arn = aws_iam_role.example.arn
   vpc_config {
-    subnet_ids = aws_subnet.public.id
+    subnet_ids = aws_subnet.public[count.index]
   }
 
 
