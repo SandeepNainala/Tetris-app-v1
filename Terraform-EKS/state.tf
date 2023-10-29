7e053c81dfa91a3eec80400d242e742daef293d2
sandeepnaidu <nainala_sandeep@yahoo.com>
2023-10-29 12:20:39 +0530

    Jenkins terraform

--- Terraform-EKS/state.tf
+++ Terraform-EKS/state.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "sandeep-mrcloudbook777" # Replace with your actual S3 bucket name
+    bucket = "sandeep-eks" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
